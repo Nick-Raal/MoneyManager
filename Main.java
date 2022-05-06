ed1c8e3e2b902bdfbd4da781d711d9d40e3e11b9
Nicholas Raal <105031104+Nick-Raal@users.noreply.github.com>
2022-05-06 17:30:51 +0000

    there are issues here

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.util.Scanner;
+import java.util.ArrayList;
 import java.time.format.DateTimeFormatter;  
 import java.time.LocalDateTime;    
 
@@ -7,7 +8,7 @@ class Main {
   public static void main(String[] args) {
     Scanner scan = new Scanner(System.in);
     while(true){
-      System.out.println("Would you like to add or withdraw money or exit the program?");
+      System.out.println("Would you like to add or withdraw money or view balance and changes (view for short) or exit the program?");
       String answer = scan.nextLine();
       if(answer.toLowerCase().equals("add")){
         try{
@@ -42,6 +43,39 @@ class Main {
       }else if(answer.toLowerCase().equals("exit")){
         System.out.println("Your data has been saved, goodbye!");
         break;
+      }else if(answer.toLowerCase().equals("view")){
+        try{
+          Scanner in = new Scanner(new File("dat.txt"));
+          double balance = 0;
+          while(in.hasNext()){
+            String nextLine = in.nextLine();
+            balance += Double.parseDouble(nextLine);
+            System.out.println(nextLine);
+          }
+          
+          System.out.println("Would you like to remove a transaction?");
+          answer = scan.nextLine();
+          if(answer.toLowerCase().equals("yes")){
+            System.out.println("Which transaction would you like to remove? (mm/dd/yy $amount)");
+            answer = scan.nextLine();
+            in.close();
+            in = new Scanner("dat.txt");
+            FileWriter fw = new FileWriter("dat.txt", false);
+            ArrayList<String> list = new ArrayList<String>();
+            while(in.hasNext()){
+              list.add(in.nextLine());
+            }
+            for(int i = 0; i < list.size(); i++){
+              if(!list.get(i).equals(answer)){
+                fw.write(list.get(i));
+              }
+            }
+            fw.close();
+          }
+          in.close();
+        }catch(Exception e){
+          System.out.println(e.getMessage());
+        }
       }else{
         System.out.println("Invalid Input");
       }
